c5bb4f04fc72fec842682c6ee12c29dee2b63c4c
fecx-hbrs <felipe.camus@hotmail.de>
2024-04-26 17:20:50 +0200

    added folders again

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu               # ask for a node with 4 GPUs
 #SBATCH --time=3:24:00           # total runtime of job allocation
 
-cd /home/fcamus2s/bachelor_thesis
+cd /home/fcamus2s/bachelor_thesis/
 git pull
 module load cuda
 #./my_cuda_program.exe
