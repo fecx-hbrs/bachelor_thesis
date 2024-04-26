9c75ada00e1decfb03bd91b159d94479d7c47286
fecx-hbrs <felipe.camus@hotmail.de>
2024-04-26 16:40:00 +0200

    added run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --partition=gpu          # GPU partition
+#SBATCH --nodes=1                # number of nodes
+#SBATCH --gres=gpu               # ask for a node with 4 GPUs
+#SBATCH --time=3:24:00           # total runtime of job allocation
+
+module load cuda
+#./my_cuda_program.exe
+python PGTSP20.py --search-opt 2
